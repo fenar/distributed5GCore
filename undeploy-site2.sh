8faf1217709920e7f87a10250a0f9d6c21686960
Fatih Nar <fenar@yahoo.com>
2022-05-24 13:37:47 -0500

    Update undeploy-site2.sh

--- undeploy-site2.sh
+++ undeploy-site2.sh
@@ -5,4 +5,7 @@ echo
 oc project prod2-5gcore
 ./site2/undeploy-prod2-5gcore.sh
 #oc delete project prod2-5gcore
+log "Uninstalling control plane for prod2-mesh"
+oc delete -f site2/smcp.yaml
+oc delete -f site2/smmr.yaml
 echo "The End"
