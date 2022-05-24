a84145700c11467106673a642ba8325f6d0b1fd7
Fatih Nar <fenar@yahoo.com>
2022-05-23 22:34:01 -0500

    Update undeploy.sh

--- undeploy.sh
+++ undeploy.sh
@@ -2,6 +2,8 @@
 #Author: fenar
 echo -e "World is About to End!....\n"
 echo
+oc project prod1-5gcore
 ./site1/undeploy-prod1-5gcore.sh
+oc project prod2-5gcore
 ./site2/undeploy-prod2-5gcore.sh
 echo "The End"
