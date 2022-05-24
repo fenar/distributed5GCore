1822505621aef7fcc545853cc1fe707ee3828d73
Fatih Nar <fenar@yahoo.com>
2022-05-24 11:08:47 -0500

    Update and rename 9-undeploy.sh to undeploy-site1.sh

--- undeploy-site1.sh
+++ undeploy-site1.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 #Author: fenar
-echo -e "World is About to End!....\n"
+echo -e "Site1 is getting Destroyed!....\n"
 echo
 oc project prod1-5gcore
 ./site1/undeploy-prod1-5gcore.sh
 #oc delete project prod1-5gcore
-oc project prod2-5gcore
-./site2/undeploy-prod2-5gcore.sh
-#oc delete project prod2-5gcore
 echo "The End"
