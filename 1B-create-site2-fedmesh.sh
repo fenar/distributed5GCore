dbc971c0598d48a6f9de2b9299fc2ae7ecf5ab4a
Fatih Nar <fenar@yahoo.com>
2022-05-24 12:11:45 -0500

    Update 1B-create-site2-fedmesh.sh

--- 1B-create-site2-fedmesh.sh
+++ 1B-create-site2-fedmesh.sh
@@ -11,6 +11,10 @@ PROD1_MESH_CERT=`cat PROD1_MESH_CERT.TXT`
 
 # Enable federation for site2
 cp site2/mesh-ca-root-cert.bak site2/prod1meshcarootcert.yaml
+sed "s:<PROD1_MESH_CERT>:$PROD1_MESH_CERT:g" site2/prod1meshcarootcert.yaml | oc apply -f -
+oc apply -f site1/smp.yaml
+oc apply -f site1/iss.yaml
+
 sed -e "s:<PROD1_MESH_CERT>:$PROD1_MESH_CERT:g" site2/prod1meshcarootcert.yaml
 oc apply -f site2/prod1meshcarootcert.yaml
 oc apply -f site2/smp.yaml
