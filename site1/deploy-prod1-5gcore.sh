f42d61c62c02dbabd1eb2d0633b9da37578d943b
Fatih Nar <fenar@yahoo.com>
2022-05-23 19:32:00 -0500

    Update deploy-prod1-5gcore.sh

--- site1/deploy-prod1-5gcore.sh
+++ site1/deploy-prod1-5gcore.sh
@@ -3,6 +3,7 @@
 current_dir=$PWD
 oc create secret generic mongodb-ca --from-file=$current_dir/cnfs/ca-tls-certificates/rds-combined-ca-bundle.pem
 oc get secret
+cd cnfs
 echo "Deploying Open5G Core Prod1"
 helm install -f values.yaml prod1-5gcore ./
-echo "Enjoy The Open 5G Core on Production 1!"
+echo "Enjoy The Open 5G Core on Production Site1!"
