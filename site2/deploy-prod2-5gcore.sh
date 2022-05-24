6002912f9cf98c67767918c5f3d11b0751d96e20
Fatih Nar <fenar@yahoo.com>
2022-05-23 20:32:00 -0500

    Update deploy-prod2-5gcore.sh

--- site2/deploy-prod2-5gcore.sh
+++ site2/deploy-prod2-5gcore.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #Author: fenar
-cd cnfs
+current_dir=$PWD
+cd $current_dir/site2/cnfs
 echo "Deploying Open5G Core Production Site2"
 helm install -f values.yaml prod2-5gcore ./
 echo "Enjoy The Open 5G Core on Production Site2!"
