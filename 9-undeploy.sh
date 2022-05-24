050213c8ca99dcfa03255daa179524f9d20b2350
Fatih Nar <fenar@yahoo.com>
2022-05-24 01:03:20 -0500

    Update 9-undeploy.sh

--- 9-undeploy.sh
+++ 9-undeploy.sh
@@ -4,8 +4,8 @@ echo -e "World is About to End!....\n"
 echo
 oc project prod1-5gcore
 ./site1/undeploy-prod1-5gcore.sh
-oc delete project prod1-5gcore
+#oc delete project prod1-5gcore
 oc project prod2-5gcore
 ./site2/undeploy-prod2-5gcore.sh
-oc delete project prod2-5gcore
+#oc delete project prod2-5gcore
 echo "The End"
