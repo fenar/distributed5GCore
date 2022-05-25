7f078352949d9850e74082fd809b2880220e7ba1
Fatih Nar <fenar@yahoo.com>
2022-05-25 12:19:16 -0500

    Update 9A-undeploy-site1.sh

--- 9A-undeploy-site1.sh
+++ 9A-undeploy-site1.sh
@@ -4,8 +4,6 @@ echo -e "Site1 is getting Destroyed!....\n"
 echo
 oc project prod1-5gcore
 ./site1/undeploy-prod1-5gcore.sh
-#oc delete project prod1-5gcore
-log "Uninstalling control plane for prod1-mesh"
 oc delete -f site1/smcp.yaml
 oc delete -f site1/smmr.yaml
 echo "The End"
