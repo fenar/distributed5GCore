dc211b33120f8bc31658f4f7c0afd068b64cc42f
Fatih Nar <fenar@yahoo.com>
2022-05-25 12:19:26 -0500

    Update 9B-undeploy-site2.sh

--- 9B-undeploy-site2.sh
+++ 9B-undeploy-site2.sh
@@ -4,8 +4,6 @@ echo -e "Site2 is About to End!....\n"
 echo
 oc project prod2-5gcore
 ./site2/undeploy-prod2-5gcore.sh
-#oc delete project prod2-5gcore
-log "Uninstalling control plane for prod2-mesh"
 oc delete -f site2/smcp.yaml
 oc delete -f site2/smmr.yaml
 echo "The End"
