6fb81507cb5350b569b351ce216f1c68441867b1
FATIH NAR <fenar@FATIHs-Mac-Pro.attlocal.net>
2022-05-26 08:04:14 -0500

    5gran addition

---
+++ 2B-Deploy5GRAN.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#Author: fenar
+oc new-project open5gran
+oc adm policy add-scc-to-user anyuid -z default -n open5gran
+oc adm policy add-scc-to-user hostaccess -z default -n open5gran
+oc adm policy add-scc-to-user hostmount-anyuid -z default -n open5gran
+oc adm policy add-scc-to-user privileged -z default -n open5gran
+cd 5gran
+## gNB Section
+echo "Preparing gNB config"
+oc get services -n prod2-5gcore | grep amf-open5gs-sctp | awk '{print $3}' > amf-ip
+echo "AMF IP:" && cat amf-ip
+cp templates/5gran-gnb-configmap.bak templates/5gran-gnb-configmap.yaml
+cp templates/5gran-ue-configmap.bak templates/5gran-ue-configmap.yaml
+sed -e "s/<put-your-amf-service-ip-here>/$(<amf-ip sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i templates/5gran-gnb-configmap.yaml
+echo "gNB Config:" && cat templates/5gran-gnb-configmap.yaml
+helm install -f values.yaml 5gran2 ./
+echo "Enjoy The 5GRAN!"
