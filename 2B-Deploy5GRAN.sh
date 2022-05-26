f1eb450f42527a1854e18a2fbf564bd02fba07d6
Fatih Nar <fenar@yahoo.com>
2022-05-26 08:29:48 -0500

    Update 2B-Deploy5GRAN.sh

--- 2B-Deploy5GRAN.sh
+++ 2B-Deploy5GRAN.sh
@@ -8,8 +8,8 @@ oc adm policy add-scc-to-user privileged -z default -n open5gran
 cd 5gran
 ## gNB Section
 echo "Preparing gNB config"
-oc get services -n prod2-5gcore | grep amf-open5gs-sctp | awk '{print $3}' > amf-ip
-echo "AMF IP:" && cat amf-ip
+oc get nodes -o wide | grep worker | awk '{print $6}' | head -1 > amf-ip
+echo "Worker Node IP:" && cat amf-ip
 cp templates/5gran-gnb-configmap.bak templates/5gran-gnb-configmap.yaml
 cp templates/5gran-ue-configmap.bak templates/5gran-ue-configmap.yaml
 sed -e "s/<put-your-amf-service-ip-here>/$(<amf-ip sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i templates/5gran-gnb-configmap.yaml
