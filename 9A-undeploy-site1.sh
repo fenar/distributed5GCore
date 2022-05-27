e5e05e56afa6d4793a2c2c7dc5449cff2b55e68b
Fatih Nar <fenar@yahoo.com>
2022-05-27 12:25:22 -0500

    Update 9A-undeploy-site1.sh

--- 9A-undeploy-site1.sh
+++ 9A-undeploy-site1.sh
@@ -5,6 +5,7 @@ echo
 oc project prod1-5gcore
 ./site1/undeploy-prod1-5gcore.sh
 oc delete -f site1/open5gcorewebui-expose.yaml
+oc delete -n prod1-5gcore -f site1/virtservices-distribute-details.yaml
 oc delete -f site1/smcp.yaml
 oc delete -f site1/smmr.yaml
 oc delete -f site2/smp.yaml
