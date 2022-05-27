e2c0a41af3441a81d428f45de17b1da7ae96690c
Fatih Nar <fenar@yahoo.com>
2022-05-27 12:23:55 -0500

    Update 9A-undeploy-site1.sh

--- 9A-undeploy-site1.sh
+++ 9A-undeploy-site1.sh
@@ -6,4 +6,6 @@ oc project prod1-5gcore
 ./site1/undeploy-prod1-5gcore.sh
 oc delete -f site1/smcp.yaml
 oc delete -f site1/smmr.yaml
+oc delete -f site2/smp.yaml
+oc delete -f site2/ess.yaml
 echo "The End"
