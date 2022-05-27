ab34aa78b1c288d8f148f0d28ddc2087543e2b82
Fatih Nar <fenar@yahoo.com>
2022-05-27 12:24:41 -0500

    Update 9A-undeploy-site1.sh

--- 9A-undeploy-site1.sh
+++ 9A-undeploy-site1.sh
@@ -4,6 +4,7 @@ echo -e "Site1 is getting Destroyed!....\n"
 echo
 oc project prod1-5gcore
 ./site1/undeploy-prod1-5gcore.sh
+oc delete -f site1/open5gcorewebui-expose.yaml
 oc delete -f site1/smcp.yaml
 oc delete -f site1/smmr.yaml
 oc delete -f site2/smp.yaml
