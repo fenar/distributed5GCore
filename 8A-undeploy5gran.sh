e00f3d24e6600b1a69311c2fedfe2eed2b3a559a
fenar <fenar@jumpserver.narlabs.io>
2022-05-31 15:01:39 -0500

    revise site2 base

---
+++ 8A-undeploy5gran.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#Author: fenar
+echo -e "Wiping UERANSIM....\n"
+echo
+cd 5gran
+rm amf-ip
+echo "Removing UERANSIM Deployment"
+rm templates/5gran-gnb-configmap.yaml
+helm uninstall 5gran-prod1
+echo "Bye"
